48ccb82e171c2c426608debd250f55bbaada764b
ansh634 <anshagrawal634@gmail.com>
2023-11-17 21:30:37 +0530

    Add factorial code

---
+++ practicals/factorial.php
@@ -0,0 +1,17 @@
+<?php
+// Function to calculate the factorial
+function factorial($n) {
+    if ($n === 0 || $n === 1) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+// Define the number for which you want to find the factorial
+$number = 5;
+$result = factorial($number);
+
+// Display the factorial
+echo "Factorial of $number is: $result\n";
+?>
