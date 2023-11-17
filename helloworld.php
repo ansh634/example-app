cbf416e8e64bd17b00feb3e053c335f40ffb0c13
ansh634 <anshagrawal634@gmail.com>
2023-11-17 21:54:19 +0530

    Add song class

---
+++ helloworld.php
@@ -0,0 +1,9 @@
+<?php
+// This PHP script prints "Hello, World!" to the console
+
+// Define a variable to store the message
+$greetingMessage = "Hello, World!";
+
+// Print the greeting message
+echo $greetingMessage;
+
---
+++ practicals/song.php
@@ -0,0 +1,51 @@
+<?php
+// Define a Song class
+class Song {
+    // Private properties
+    private $title;
+    private $artist;
+    private $genre;
+    private $tempo;
+
+    // Setter for title property
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    // Getter for title property
+    public function getTitle() {
+        return $this->title;
+    }
+
+    // Setter for artist property
+    public function setArtist($artist) {
+        $this->artist = $artist;
+    }
+
+    // Getter for artist property
+    public function getArtist() {
+        return $this->artist;
+    }
+
+    // Setter for genre property
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    // Getter for genre property
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    // Setter for tempo property
+    public function setTempo($tempo) {
+        $this->tempo = $tempo;
+    }
+
+    // Getter for tempo property
+    public function getTempo() {
+        return $this->tempo;
+    }
+}
+?>
+
