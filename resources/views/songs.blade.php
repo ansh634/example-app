4954ba4be9296bf9d3b797b54b25864d14076895
ansh634 <anshagrawal634@gmail.com>
2023-11-18 14:35:28 +0530

    Render songs from Song objects

---
+++ resources/views/songs.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<head>
+
+    <title>Songs Page</title>
+</head>
+<body>
+<table>
+<tr>
+<th>Title</th>
+<th>Artist</th>
+</tr>
+@foreach ($songs as $song)
+<tr>
+<td>{{ $song->getTitle() }}</td>
+<td>{{ $song->getArtist() }}</td>
+</tr>
+@endforeach
+</table>
+</body>
+</html>
